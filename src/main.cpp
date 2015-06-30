d0c77cb3ed0e545251eb3b07e6adc393d2418a23
Tammo Behrends <behr.tam@gmail.com>
2015-06-30 22:20:58 +0200

    Added organization name and application name

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
+    app.setOrganizationName("HTW MM15 QtProject");
+    app.setApplicationName("Reversi GUI");
     MainWindow mainWin;
     mainWin.show();
     return app.exec();
