431ccf107f9204062738e409d3a04e1bcc9dccd3
Tammo Behrends <behr.tam@gmail.com>
2015-07-04 12:51:18 +0200

    Added enum that represents the sql table columns

---
+++ src/highscore_columns.h
@@ -0,0 +1,10 @@
+// Copyright 2015 Tammo Behrends
+
+#ifndef REVERSI_HIGHSCORE_COLUMNS_H
+#define REVERSI_HIGHSCORE_COLUMNS_H
+
+
+enum class Columns { start, end, boardsize, moves , blackname, blackstones, whitename, whitestones };
+
+
+#endif  // REVERSI_HIGHSCORE_COLUMNS_H
