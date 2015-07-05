9de94e8e568c661aac88d515f547f530934a75c5
Tammo Behrends <behr.tam@gmail.com>
2015-07-05 14:12:08 +0200

    Added db connection check before app start

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,17 @@
 #include <QApplication>
 
 #include "./mainwindow.h"
+#include "./connect.h"
+
 
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
     app.setOrganizationName("HTW MM15 QtProject");
     app.setApplicationName("Reversi GUI");
+
+    if (!createConnection())
+        return 1;
+
     MainWindow mainWin;
     mainWin.show();
     return app.exec();
