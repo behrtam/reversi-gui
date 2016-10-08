593fb4a14cd628c64d56a78b95a177d0ef990722
Tammo Behrends <behr.tam@gmail.com>
2016-10-09 00:07:18 +0200

    Update translation files

--- languages/test_de.ts
+++ languages/test_de.ts
@@ -4,43 +4,48 @@
 <context>
     <name>HighScoreDialog</name>
     <message>
-        <location filename="../src/highscore_dialog.cpp" line="11"/>
+        <location filename="../src/highscore_dialog.cpp" line="17"/>
         <source>Start date</source>
         <translation>Startdatum</translation>
     </message>
     <message>
-        <location filename="../src/highscore_dialog.cpp" line="12"/>
+        <location filename="../src/highscore_dialog.cpp" line="18"/>
         <source>End date</source>
         <translation>Enddatum</translation>
     </message>
     <message>
-        <location filename="../src/highscore_dialog.cpp" line="13"/>
+        <location filename="../src/highscore_dialog.cpp" line="19"/>
         <source>Board</source>
         <translation>Brett</translation>
     </message>
     <message>
-        <location filename="../src/highscore_dialog.cpp" line="14"/>
-        <source>Move</source>
-        <translation>Zug</translation>
+        <location filename="../src/highscore_dialog.cpp" line="20"/>
+        <source>Moves</source>
+        <translation>Züge</translation>
     </message>
     <message>
-        <location filename="../src/highscore_dialog.cpp" line="15"/>
+        <location filename="../src/highscore_dialog.cpp" line="22"/>
+        <location filename="../src/highscore_dialog.cpp" line="24"/>
+        <source>Stones</source>
+        <translation>Steine</translation>
+    </message>
+    <message>
+        <location filename="../src/highscore_dialog.cpp" line="21"/>
         <source>Black</source>
         <translation>Schwarz</translation>
     </message>
     <message>
-        <location filename="../src/highscore_dialog.cpp" line="16"/>
-        <location filename="../src/highscore_dialog.cpp" line="18"/>
-        <source>Stone</source>
-        <translation>Stein</translation>
-    </message>
-    <message>
-        <location filename="../src/highscore_dialog.cpp" line="17"/>
+        <location filename="../src/highscore_dialog.cpp" line="23"/>
         <source>White</source>
         <translation>Weiß</translation>
     </message>
     <message>
-        <location filename="../src/highscore_dialog.cpp" line="40"/>
+        <location filename="../src/highscore_dialog.cpp" line="38"/>
+        <source>Print</source>
+        <translation>Drucken</translation>
+    </message>
+    <message>
+        <location filename="../src/highscore_dialog.cpp" line="50"/>
         <source>High Score Dialog</source>
         <translation>Highscore Dialog</translation>
     </message>
@@ -48,243 +53,282 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../src/mainwindow.cpp" line="67"/>
-        <source>Status Bar</source>
-        <translation>Status Leiste</translation>
-    </message>
-    <message>
-        <location filename="../src/mainwindow.cpp" line="93"/>
+        <location filename="../src/mainwindow.cpp" line="97"/>
         <source>Reversi Main Window</source>
         <translation>Reversi Hauptfenster</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="262"/>
+        <location filename="../src/mainwindow.cpp" line="313"/>
         <source>Restart</source>
         <translation>Neu Start</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="102"/>
-        <location filename="../src/mainwindow.cpp" line="306"/>
+        <location filename="../src/mainwindow.cpp" line="357"/>
         <source>&amp;New game</source>
         <translation>Neues Spiel</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="317"/>
+        <location filename="../src/mainwindow.cpp" line="368"/>
         <source>E&amp;xit</source>
         <translation>Beenden</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="322"/>
+        <location filename="../src/mainwindow.cpp" line="373"/>
         <source>&amp;Load game</source>
         <translation>Spiel laden</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="324"/>
+        <location filename="../src/mainwindow.cpp" line="375"/>
         <source>Load the saved game</source>
         <translation>Lade das gespeicherte Spiel</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="327"/>
+        <location filename="../src/mainwindow.cpp" line="378"/>
         <source>&amp;Save game</source>
         <translation>Spiel speichern</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="245"/>
+        <location filename="../src/mainwindow.cpp" line="290"/>
         <source>The Game is over!</source>
-        <translation type="unfinished"></translation>
+        <translation>Das Spiel ist vorbei!</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="250"/>
+        <location filename="../src/mainwindow.cpp" line="190"/>
+        <source>Undo</source>
+        <translation>Rückgängig</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="195"/>
+        <source>Stop Computer</source>
+        <translation>Computer stoppen</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="295"/>
         <source>Game is over!</source>
-        <translation type="unfinished"></translation>
+        <translation>Spiel ist vorbei!</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="329"/>
+        <location filename="../src/mainwindow.cpp" line="308"/>
+        <source>It&apos;s a draw!</source>
+        <translation>Unendschieden</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="380"/>
         <source>Save the current game</source>
         <translation>Speicher das aktuelle Spiel</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="332"/>
+        <location filename="../src/mainwindow.cpp" line="383"/>
         <source>&amp;Clear saved game</source>
-        <translation type="unfinished"></translation>
+        <translation>Gespeichertes Spiel löschen</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="333"/>
+        <location filename="../src/mainwindow.cpp" line="384"/>
         <source>Clear the saved game</source>
-        <translation type="unfinished"></translation>
+        <translation>Gespeichertes Spiel löschen</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="336"/>
+        <location filename="../src/mainwindow.cpp" line="387"/>
         <source>&amp;Show high score</source>
         <translation>Highscore anzeigen</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="337"/>
+        <location filename="../src/mainwindow.cpp" line="388"/>
         <source>Shows the saved scores</source>
-        <translation type="unfinished"></translation>
+        <translation>Gespeicherte Spielstände anzeigen</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="343"/>
+        <location filename="../src/mainwindow.cpp" line="394"/>
         <source>&amp;Clear high score</source>
-        <translation type="unfinished"></translation>
+        <translation>Highscore löschen</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="344"/>
+        <location filename="../src/mainwindow.cpp" line="395"/>
         <source>Delete all the saved scores</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle gespeicherten Spielstände löschen</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="358"/>
+        <location filename="../src/mainwindow.cpp" line="404"/>
         <source>4x4</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="360"/>
+        <location filename="../src/mainwindow.cpp" line="406"/>
         <source>Change board size to 4x4</source>
-        <translation type="unfinished"></translation>
+        <translation>Spielfeld in ändern in 4x4</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="363"/>
+        <location filename="../src/mainwindow.cpp" line="409"/>
         <source>6x6</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="365"/>
+        <location filename="../src/mainwindow.cpp" line="411"/>
         <source>Change board size to 6x6</source>
-        <translation type="unfinished"></translation>
+        <translation>Spielfeld in ändern in 6x6</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="368"/>
+        <location filename="../src/mainwindow.cpp" line="414"/>
         <source>8x8</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="370"/>
+        <location filename="../src/mainwindow.cpp" line="416"/>
         <source>Change board size to 8x8</source>
-        <translation type="unfinished"></translation>
+        <translation>Spielfeld in ändern in 8x8</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="373"/>
+        <location filename="../src/mainwindow.cpp" line="419"/>
         <source>10x10</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="375"/>
+        <location filename="../src/mainwindow.cpp" line="421"/>
         <source>Change board size to 10x10</source>
-        <translation type="unfinished"></translation>
+        <translation>Spielfeld in ändern in 10x10</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="378"/>
+        <location filename="../src/mainwindow.cpp" line="424"/>
         <source>12x12</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="380"/>
+        <location filename="../src/mainwindow.cpp" line="426"/>
         <source>Change board size to 12x12</source>
-        <translation type="unfinished"></translation>
+        <translation>Spielfeld in ändern in 12x12</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="391"/>
+        <location filename="../src/mainwindow.cpp" line="437"/>
         <source>human</source>
-        <translation type="unfinished"></translation>
+        <translation>Mensch</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="393"/>
+        <location filename="../src/mainwindow.cpp" line="439"/>
         <source>Change black player to be human</source>
-        <translation type="unfinished"></translation>
+        <translation>Schwarz in Mensch umstellen</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="396"/>
+        <location filename="../src/mainwindow.cpp" line="442"/>
         <source>random</source>
-        <translation type="unfinished"></translation>
+        <translation>Zufall</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="398"/>
+        <location filename="../src/mainwindow.cpp" line="444"/>
         <source>Change black player to be random</source>
-        <translation type="unfinished"></translation>
+        <translation>Schwarz in Computer umstellen</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="407"/>
+        <location filename="../src/mainwindow.cpp" line="452"/>
         <source>en</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="409"/>
+        <location filename="../src/mainwindow.cpp" line="454"/>
         <source>Change language to EN</source>
-        <translation type="unfinished"></translation>
+        <translation>Sprache in EN umstellen</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="414"/>
+        <location filename="../src/mainwindow.cpp" line="459"/>
         <source>Change language to DE</source>
-        <translation type="unfinished"></translation>
+        <translation>Sprache in DE umstellen</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="412"/>
+        <location filename="../src/mainwindow.cpp" line="575"/>
+        <source>Reset running game.</source>
+        <translation>Laufendes Spiel zurrücksetzten</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="576"/>
+        <source>Do you want to reset your game to apply changes?</source>
+        <translation>Wollen Sie ihr Spiel zurrücksetzten?</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="598"/>
+        <source>Save running game.</source>
+        <translation>Laufendes Spiel speichern</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="599"/>
+        <source>Do you want to save your running game?</source>
+        <translation>Wollen Sie das laufende Spiel speichern</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="457"/>
         <source>de</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="422"/>
+        <location filename="../src/mainwindow.cpp" line="474"/>
         <source>Black starts</source>
-        <translation>Schwarz begeinnt</translation>
+        <translation>Schwarz beginnt</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="100"/>
-        <location filename="../src/mainwindow.cpp" line="432"/>
-        <location filename="../src/mainwindow.cpp" line="486"/>
+        <location filename="../src/mainwindow.cpp" line="104"/>
+        <location filename="../src/mainwindow.cpp" line="484"/>
+        <location filename="../src/mainwindow.cpp" line="540"/>
         <source>&amp;About</source>
         <translation>Info</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="433"/>
+        <location filename="../src/mainwindow.cpp" line="485"/>
         <source>Show the application&apos;s About box</source>
-        <translation type="unfinished"></translation>
+        <translation>Info anzeigen</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="94"/>
-        <location filename="../src/mainwindow.cpp" line="453"/>
+        <location filename="../src/mainwindow.cpp" line="98"/>
+        <location filename="../src/mainwindow.cpp" line="505"/>
         <source>&amp;File</source>
         <translation>&amp;Datei</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="95"/>
-        <location filename="../src/mainwindow.cpp" line="462"/>
+        <location filename="../src/mainwindow.cpp" line="99"/>
+        <location filename="../src/mainwindow.cpp" line="514"/>
         <source>&amp;Settings</source>
         <translation>Einstellungen</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="96"/>
-        <location filename="../src/mainwindow.cpp" line="465"/>
+        <location filename="../src/mainwindow.cpp" line="100"/>
+        <location filename="../src/mainwindow.cpp" line="517"/>
         <source>&amp;Board size</source>
         <translation>Brettgröße</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="97"/>
-        <location filename="../src/mainwindow.cpp" line="472"/>
+        <location filename="../src/mainwindow.cpp" line="101"/>
+        <location filename="../src/mainwindow.cpp" line="524"/>
         <source>&amp;Black player</source>
         <translation>Schwarzer Spieler</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="98"/>
-        <location filename="../src/mainwindow.cpp" line="476"/>
+        <location filename="../src/mainwindow.cpp" line="102"/>
+        <location filename="../src/mainwindow.cpp" line="528"/>
         <source>&amp;Languages</source>
         <translation>Sprachen</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="99"/>
-        <location filename="../src/mainwindow.cpp" line="482"/>
+        <location filename="../src/mainwindow.cpp" line="103"/>
+        <location filename="../src/mainwindow.cpp" line="536"/>
         <source>&amp;High score</source>
         <translation>&amp;Highscore</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="494"/>
-        <source>About Reversi</source>
-        <translation type="unfinished"></translation>
+        <location filename="../src/mainwindow.cpp" line="467"/>
+        <source>Play sounds</source>
+        <translation>Geräsuche abspielen</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="495"/>
+        <location filename="../src/mainwindow.cpp" line="471"/>
+        <source>Change Names</source>
+        <translation>Namen ändern</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="548"/>
+        <source>About Reversi</source>
+        <translation>Über Reversi</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="549"/>
         <source>The &lt;b&gt;Reversi&lt;/b&gt; game demonstrates how to write a simple board game in C++14 with a GUI using Qt 5.</source>
-        <translation>Das Spiel Reversi ...</translation>
+        <translation>Das Spiel Reversi zeigt wie man ein einfaches Brettspiel in C++12 mit einer GUI in Qt5 umsetzten kann.</translation>
     </message>
 </context>
 </TS>
