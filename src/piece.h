6f6eba791b22f759f90d92bbccabaa3748a99e9d
Tammo Behrends <behr.tam@gmail.com>
2016-10-09 00:18:05 +0200

    Add docs for enum Piece

--- src/piece.h
+++ src/piece.h
@@ -11,15 +11,31 @@
 #include "./utils.h"
 
 
+/**
+ *  The enum Piece represents a field on the ReversiGame board, which can be black, white or empty.
+ */
 enum class Piece {
-    empty, black, white
+    empty, ///< Piece empty
+    black, ///< Piece black
+    white ///< Piece white
 };
 
+/**
+ *  Writes a textual representation of the given piece to the given stream.
+ * \param os   the output stream.
+ * \param p     the piece to be written.
+ * \return a reference to the used stream.
+ */
 inline std::ostream &operator<<(std::ostream &os, const Piece &p) {
     static std::array<std::string, 3> piece_strings{{"_", "X", "O"}};
     return os << piece_strings[as_integer(p)];
 }
 
+/**
+ * Inverts the given Piece (only if it's not empty).
+ * @param p the pice.
+ * @return the inverted piece.
+ */
 inline Piece invert(const Piece p) {
     if (p == Piece::black)
         return Piece::white;
@@ -28,6 +44,11 @@ inline Piece invert(const Piece p) {
     return Piece::empty;
 }
 
+/**
+ * Inverts the given Piece with the ! operator (only if it's not empty).
+ * @param p the pice.
+ * @return the inverted piece.
+ */
 inline Piece operator!(Piece p) {
     return invert(p);
 }
